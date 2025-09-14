1a52b9c3565b4650ecb9f4817353f8927559ba30
Chris Serpico <christopherserpico@gmail.com>
2025-09-14 17:09:45 -0400

    Add recipe to craft Glistering Melon from Melon

--- src/main/java/com/prollumsloof/brighterglistering/datagen/ModRecipeProvider.java
+++ src/main/java/com/prollumsloof/brighterglistering/datagen/ModRecipeProvider.java
@@ -5,6 +5,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -23,6 +24,17 @@ public class ModRecipeProvider extends RecipeProvider {
                 .unlockedBy("has_glistering_melon_slice", has(Items.GLISTERING_MELON_SLICE))
                 .unlockedBy("has_glistering_melon", has(BrighterGlistering.GLISTERING_MELON))
                 .save(recipeOutput);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, BrighterGlistering.GLISTERING_MELON)
+                .pattern("GGG")
+                .pattern("GMG")
+                .pattern("GGG")
+                .define('G', Items.GOLD_INGOT)
+                .define('M', Blocks.MELON)
+                .unlockedBy("has_gold_ingot", has(Items.GOLD_INGOT))
+                .unlockedBy("has_melon", has(Blocks.MELON))
+                .unlockedBy("has_glistering_melon", has(BrighterGlistering.GLISTERING_MELON))
+                .save(recipeOutput, "glistering_melon_from_melon");
+
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Items.GLISTERING_MELON_SLICE, 9)
                 .requires(BrighterGlistering.GLISTERING_MELON)
                 .unlockedBy("has_glistering_melon", has(BrighterGlistering.GLISTERING_MELON))
