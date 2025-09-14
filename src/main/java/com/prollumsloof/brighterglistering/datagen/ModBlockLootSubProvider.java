ea81fc9a6b5d0c20f681fe52d3bb32e80ad6d9ef
Chris Serpico <christopherserpico@gmail.com>
2025-09-14 17:08:19 -0400

    Use datagen for melon stem loot table

--- src/main/java/com/prollumsloof/brighterglistering/datagen/ModBlockLootSubProvider.java
+++ src/main/java/com/prollumsloof/brighterglistering/datagen/ModBlockLootSubProvider.java
@@ -4,6 +4,7 @@ import com.prollumsloof.brighterglistering.BrighterGlistering;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 
 import java.util.Set;
@@ -16,6 +17,7 @@ public class ModBlockLootSubProvider extends BlockLootSubProvider {
     @Override
     protected void generate() {
         dropSelf(BrighterGlistering.GLISTERING_MELON.get());
+        add(BrighterGlistering.ATTACHED_GLISTERING_MELON_STEM.get(), createAttachedStemDrops(BrighterGlistering.ATTACHED_GLISTERING_MELON_STEM.get(), Items.MELON_SEEDS));
     }
 
     @Override
